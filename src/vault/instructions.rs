f972a629cda6c4b1ababbf2ca0ab50f34e15ebaa
suite <gr8bitmail@gmail.com>
2025-02-23 19:01:49 -0700

    update idl; update fill; started swap

--- src/vault/instructions.rs
+++ src/vault/instructions.rs
@@ -7,7 +7,7 @@ use anchor_spl::token::spl_token;
 use std::rc::Rc;
 
 use crate::memepool;
-use super::utils::{VAULT_PDA, MEME_MINT_PDA};
+use super::utils::{get_oracle_pda, CP_SWAP_PROGRAM, MEME_MINT_PDA, POOL_ADDRESS, SWAP_AUTHORITY_PDA, TEST_TOKEN_MINT, VAULT_PDA, WSOL_MINT};
 
 pub async fn fill_withdraw_request(
     program: &Program<Rc<Keypair>>,
@@ -18,10 +18,21 @@ pub async fn fill_withdraw_request(
 ) -> Result<String, String> {
     let vault_address = *VAULT_PDA;
     let meme_mint = *MEME_MINT_PDA;
+    let wsol_mint = WSOL_MINT;
     
     let withdraw_request_meme_ata = anchor_spl::associated_token::get_associated_token_address(
         &request_pubkey,
         &meme_mint,
+    );
+
+    let vault_wsol_ata = anchor_spl::associated_token::get_associated_token_address(
+        &vault_address,
+        &wsol_mint,
+    );
+
+    let temp_vault_wsol_ata = anchor_spl::associated_token::get_associated_token_address(
+        &request_pubkey,
+        &wsol_mint,
     );
 
     let accounts = memepool::client::accounts::VaultFillWithdraw {
@@ -29,10 +40,14 @@ pub async fn fill_withdraw_request(
         withdrawer: withdraw_request.user,
         withdraw_request: request_pubkey,
         vault: vault_address,
-        meme_mint: meme_mint,
+        meme_mint,
         withdraw_request_meme_ata,
+        wsol_mint,
+        vault_wsol_ata,
+        temp_vault_wsol_ata,
         system_program: system_program::ID,
         token_program: spl_token::ID,
+        associated_token_program: anchor_spl::associated_token::ID,
     };
 
     let args = memepool::client::args::VaultFillWithdraw { fill_lamports };
@@ -48,4 +63,76 @@ pub async fn fill_withdraw_request(
         .map_err(|e| format!("Failed to send fill withdraw transaction: {}", e))?;
 
     Ok(tx.to_string())
-} 
+}
+
+pub async fn lp_swap(
+    program: &Program<Rc<Keypair>>,
+    aggregator_keypair: &Keypair,
+    amount_in: u64,
+    minimum_amount_out: u64,
+) -> Result<String, String> {
+    let vault_address = *VAULT_PDA;
+    let cp_swap_program = CP_SWAP_PROGRAM;
+    let wsol_mint = WSOL_MINT;
+    let test_token = TEST_TOKEN_MINT;
+    let pool_address = POOL_ADDRESS;
+
+    // TODO: These should be fetched from pool info in the future
+    let config_id = Pubkey::try_from("11111111111111111111111111111111")
+        .map_err(|e| format!("Invalid config ID: {}", e))?;
+    let vault_a = Pubkey::try_from("11111111111111111111111111111111")
+        .map_err(|e| format!("Invalid vault A: {}", e))?;
+    let vault_b = Pubkey::try_from("11111111111111111111111111111111")
+        .map_err(|e| format!("Invalid vault B: {}", e))?;
+
+    // Get authority PDA for the swap program
+    let authority = *SWAP_AUTHORITY_PDA;
+    
+    // Get token accounts
+    let input_token_account = anchor_spl::associated_token::get_associated_token_address(
+        &vault_address,
+        &wsol_mint,
+    );
+    let output_token_account = anchor_spl::associated_token::get_associated_token_address(
+        &vault_address,
+        &test_token,
+    );
+
+    // Get oracle observation address
+    let observation_address = get_oracle_pda(&pool_address);
+
+    let accounts = memepool::client::accounts::LpSwap {
+        aggregator: aggregator_keypair.pubkey(),
+        vault: vault_address,
+        cp_swap_program,
+        authority,
+        amm_config: config_id,
+        pool_state: pool_address,
+        input_token_account,
+        output_token_account,
+        input_vault: vault_a,
+        output_vault: vault_b,
+        input_token_program: spl_token::ID,
+        output_token_program: spl_token::ID,
+        input_token_mint: wsol_mint,
+        output_token_mint: test_token,
+        observation_state: observation_address,
+    };
+
+    let args = memepool::client::args::LpSwap {
+        amount_in,
+        minimum_amount_out,
+    };
+    
+    let tx_builder = program
+        .request()
+        .args(args)
+        .accounts(accounts);
+
+    let tx = tx_builder
+        .send()
+        .await
+        .map_err(|e| format!("Failed to send swap transaction: {}", e))?;
+
+    Ok(tx.to_string())
+}
--- src/vault/utils.rs
+++ src/vault/utils.rs
@@ -1,15 +1,34 @@
-use anchor_lang::prelude::Pubkey;
+use anchor_lang::prelude::{Pubkey, pubkey};
 use crate::memepool;
 use once_cell::sync::Lazy;
 
+// Constants for swap functionality
+pub const CP_SWAP_PROGRAM: Pubkey = pubkey!("CPMDWBwJDtYax9qW7AyRuVC19Cc4L4Vcy4n2BHAbHkCW"); // DEVNET CPMM ADDRESS
+pub const WSOL_MINT: Pubkey = pubkey!("So11111111111111111111111111111111111111112");
+pub const TEST_TOKEN_MINT: Pubkey = pubkey!("DcPRHwtoWCtzt8WwtD7VdMHvMLtHya7WPknH6kmUsUbw");
+pub const POOL_ADDRESS: Pubkey = pubkey!("2zQi1M8QrJpXxLWNyBuec3N7hNG1x7DmChctYYeE5HLT"); // TODO: scan for pool ids
+
 pub static MEME_MINT_PDA: Lazy<Pubkey> = Lazy::new(|| {
-    let seeds = b"meme";
-    let (pda, _) = Pubkey::find_program_address(&[seeds], &memepool::ID);
+    let seeds: [&[u8]; 1]  = [b"meme"];
+    let (pda, _) = Pubkey::find_program_address(&seeds, &memepool::ID);
     pda
 });
 
 pub static VAULT_PDA: Lazy<Pubkey> = Lazy::new(|| {
-    let seeds = b"vault";
-    let (pda, _) = Pubkey::find_program_address(&[seeds], &memepool::ID);
+    let seeds: [&[u8]; 1]  = [b"vault"];
+    let (pda, _) = Pubkey::find_program_address(&seeds, &memepool::ID);
     pda
 });
+
+pub static SWAP_AUTHORITY_PDA: Lazy<Pubkey> = Lazy::new(|| {
+    let seeds: [&[u8]; 1] = [b"vault_and_lp_mint_auth_seed"];
+    let (pda, _) = Pubkey::find_program_address(&seeds, &CP_SWAP_PROGRAM);
+    pda
+});
+
+
+pub fn get_oracle_pda(pool: &Pubkey) -> Pubkey {
+    let seeds: [&[u8]; 2] = [b"observation".as_ref(), pool.as_ref()];
+    let (pda, _) = Pubkey::find_program_address(&seeds, &CP_SWAP_PROGRAM);
+    pda
+}
